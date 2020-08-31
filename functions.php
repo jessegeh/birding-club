32cb83fe8bbf6961053b84a308c3b47a3442a61a
Jesse Gehlen <jesse.gehlen@gmail.com>
2020-08-31 17:57:40 +0300

    added sass to project

--- functions.php
+++ functions.php
@@ -3,6 +3,14 @@
 function birding_club_files() {
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('birding_main_styles', get_stylesheet_uri());
+
+    wp_register_style('style', get_template_directory_uri() . '/dist/app.css', [], 1, 'all');
+    wp_enqueue_style('style');
+
+    wp_enqueue_script('jquery');
+
+    wp_register_script('app', get_template_directory_uri() . '/dist/app.js', ['jquery'], 1, true);
+    wp_enqueue_script('app');
 }
 
 add_action('wp_enqueue_scripts', 'birding_club_files');
